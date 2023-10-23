978bba3cca255005901fe5f600f95cc316550f7f
Thang Vu <thangvu.dona@gmail.com>
2023-10-23 15:27:13 +0700

    Tree shaking - remove unused codes on go-live.
    - named import, export
    - default import, export
    - tree-shaking with named export.

---
+++ js/api/cityApi.js
@@ -0,0 +1,36 @@
+import axiosClient from "./axiosClient";
+
+// import: default import, named import
+// export: default export, named export
+// default: can use your name alias --> have one default export ONLY
+// named export; use exactly name --> have multiple export
+
+export function getAllCities(params) {
+  const url =  '/cities';
+  return axiosClient.get(url, {params});
+};
+
+// named export
+export function getCityById(id) {
+  const url = `/cities/${id}`;
+  return axiosClient.get(url);
+};
+
+const cityApi = {
+  add(data) {
+    const url = '/cities';
+    return axiosClient.post(url, data);
+  },
+
+  update(data) {
+    const url = `/cities/${data.id}`;
+    return axiosClient.patch(url, data);
+  },
+
+  remove(id) {
+    const url = `/cities/${id}`;
+    return axiosClient.delete(url);
+  }
+}
+
+export default cityApi;
