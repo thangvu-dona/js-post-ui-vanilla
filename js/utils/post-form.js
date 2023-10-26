b41b1dd0adbb5627423fce4cf50b9e13d21a23e8
Thang Vu <thangvu.dona@gmail.com>
2023-10-27 03:28:39 +0700

    Get form values on submit
    - add event form submit
    - get field values with 2 ways
    - using FormData - WebAPI, some note on MDN
    - return a formValues object.

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -9,9 +9,40 @@ function setFormValues(form, formValues) {
   setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl);
 }
 
+function getFormValues(form) {
+  const formValues = {};
+
+  // S1: query each input and add to formValues object
+  // ['title', 'description', 'author', 'imageUrl'].forEach(name => {
+  //   const field = form.querySelector(`[name="${name}"]`);
+  //   if(field) formValues[name] = field.value;
+  // });
+
+  //S2: Using FormData - Web API
+  const data = new FormData(form);
+  for (const [key, value] of data) {
+    formValues[key] = value;
+  }
+
+  return formValues;
+}
+
 export function initPostForm({formId, defaultValues, onSubmit}) {
-  const formElement = document.getElementById(formId);
-  if (!formElement) return;
+  const form = document.getElementById(formId);
+  if (!form) return;
   
-  setFormValues(formElement, defaultValues);
+  setFormValues(form, defaultValues);
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log('submit form!');
+
+    // get form values
+    const formValues = getFormValues(form);
+    console.log(formValues);
+    // validation form values
+    // if valid --> trigger submit callback
+    // otherwise, show validation errors
+  });
 }
