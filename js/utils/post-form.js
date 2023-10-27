6538270e0a13cbb0e4a65f5ec705823fb569c6d9
Thang Vu <thangvu.dona@gmail.com>
2023-10-27 10:44:19 +0700

    Validate form with YUP
    - install yup: 'yarn add yup'
    - create yup schema
    - validate schema for each field
    - get error message from error.inner

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,16 +1,17 @@
-import { setBackgroundImage, setFieldValue } from "./common";
+import { setBackgroundImage, setFieldValue, setTextContent } from './common'
+import * as yup from 'yup'
 
 function setFormValues(form, formValues) {
-  setFieldValue(form, '[name="title"]', formValues?.title);
-  setFieldValue(form, '[name="description"]', formValues?.description);
-  setFieldValue(form, '[name="author"]', formValues?.author);
+  setFieldValue(form, '[name="title"]', formValues?.title)
+  setFieldValue(form, '[name="description"]', formValues?.description)
+  setFieldValue(form, '[name="author"]', formValues?.author)
 
-  setFieldValue(form, '[name="imageUrl"]', formValues?.imageUrl); // set to hidden field
-  setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl);
+  setFieldValue(form, '[name="imageUrl"]', formValues?.imageUrl) // set to hidden field
+  setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl)
 }
 
 function getFormValues(form) {
-  const formValues = {};
+  const formValues = {}
 
   // S1: query each input and add to formValues object
   // ['title', 'description', 'author', 'imageUrl'].forEach(name => {
@@ -19,30 +20,84 @@ function getFormValues(form) {
   // });
 
   //S2: Using FormData - Web API
-  const data = new FormData(form);
+  const data = new FormData(form)
   for (const [key, value] of data) {
-    formValues[key] = value;
+    formValues[key] = value
   }
 
-  return formValues;
+  return formValues
 }
 
-export function initPostForm({formId, defaultValues, onSubmit}) {
-  const form = document.getElementById(formId);
-  if (!form) return;
-  
-  setFormValues(form, defaultValues);
+function getPostSchema() {
+  return yup.object().shape({
+    title: yup.string().required('Please enter title'),
+    author: yup
+      .string()
+      .required('Please enter author')
+      .test(
+        'at-least-two-words',
+        'Please enter at-least two words',
+        (value) => value.split(' ').filter((x) => !!x && x.length >= 3).length >= 2,
+      ),
+    description: yup.string(),
+  })
+}
+
+function setFieldError(form, name, error) {
+  const element = form.querySelector(`[name="${name}"]`)
+  if (element) {
+    element.setCustomValidity(error)
+    setTextContent(element.parentElement, '.invalid-feedback', error)
+  }
+}
+
+async function validatePostForm(form, formValues) {
+  try {
+    // reset errors
+    ;['title', 'author'].forEach((name) => setFieldError(form, name, ''))
+
+    // start validating
+    const schema = getPostSchema()
+    await schema.validate(formValues, { abortEarly: false })
+  } catch (error) {
+    const errorLog = {}
+
+    for (const validationError of error.inner) {
+      const name = validationError.path
+
+      // ignore if the field is already logged
+      if (errorLog[name]) continue
+
+      // set field error and mark as logged
+      setFieldError(form, name, validationError.message)
+      errorLog[name] = true
+    }
+  }
+
+  // add was-validated class to form element
+  const isValid = form.checkValidity()
+  if (!isValid) form.classList.add('was-validated')
+  return isValid
+}
+
+export function initPostForm({ formId, defaultValues, onSubmit }) {
+  const form = document.getElementById(formId)
+  if (!form) return
+
+  setFormValues(form, defaultValues)
 
   form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    console.log('submit form!');
+    console.log('submit form!')
 
     // get form values
-    const formValues = getFormValues(form);
-    console.log(formValues);
+    const formValues = getFormValues(form)
+    console.log(formValues)
+
     // validation form values
     // if valid --> trigger submit callback
     // otherwise, show validation errors
-  });
+    if (!validatePostForm(form, formValues)) return
+  })
 }
